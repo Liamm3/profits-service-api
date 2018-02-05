7f869804f415c64ae3be553b92e144daa6dae0b6
Liam <lw@esyoil.com>
2018-02-05 23:20:17 +0100

    Add body-parser

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.MONGODB);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT);
 
