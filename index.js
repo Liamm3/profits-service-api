bdef3302547b2241a7dc58399c669da05792939f
Liam <lw@esyoil.com>
2018-02-05 23:31:13 +0100

    Add router to express app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const profitsRoutes = require('./profits/routes');
 
 mongoose.connect(process.env.MONGODB);
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use('/', profitsRoutes);
 
 app.listen(PORT);
 
