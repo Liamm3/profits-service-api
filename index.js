c20c4afd168b511a8019d902a430e10248b7c4ac
Liam <lw@esyoil.com>
2018-02-07 16:50:05 +0100

    Add logger

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
 const profitsRoutes = require('./profits/routes');
-
-mongoose.connect(process.env.MONGODB);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGODB);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(logger('dev'));
 app.use('/', profitsRoutes);
 
 app.listen(PORT);
